e46c9e1d53a6072c880aa5e66cd56db3f493eaa3
Moshe Immerman <moshe.immerman@gmail.com>
2019-05-08 18:24:04 +0200

    fix build

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,7 @@ TAG=$(git tag --points-at HEAD )
 GO111MODULE=off go get github.com/goreleaser/goreleaser
 git stash
 git clean -fd
-docker run --rm --privileged -v $PWD:$PWD -v /var/run/docker.sock:/var/run/docker.sock -w $PWD goreleaser/goreleaser release --rm-dist
+docker run --rm --privileged -e GITHUB_TOKEN=$GITHUB_TOKEN -v $PWD:$PWD -v /var/run/docker.sock:/var/run/docker.sock -w $PWD goreleaser/goreleaser release --rm-dist
 
 GO111MODULE=off go get github.com/aktau/github-release
 github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME} -f dist/linux_amd64/${NAME}
