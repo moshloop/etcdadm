f1750a214b061b5a43263560c8253ff04f8c5849
Moshe Immerman <moshe.immerman@gmail.com>
2019-05-08 17:21:16 +0200

    feat: release artifacts via Travis on tag

---
+++ scripts/release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+NAME=$(basename $(git remote get-url origin | sed 's/\.git//'))
+GITHUB_USER=$(basename $(dirname $(git remote get-url origin | sed 's/\.git//')))
+TAG=$(git tag --points-at HEAD )
+
+[[ -e "checkout.txt" ]] && rm checkout.txt
+go get github.com/goreleaser/goreleaser
+goreleaser release --rm-dist
+
+go get github.com/aktau/github-release
+github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME} -f dist/linux_amd64/${NAME}
+github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME}_osx -f dist/darwin_amd64/${NAME}
+github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME}.exe -f dist/windows_amd64/${NAME}.exe
