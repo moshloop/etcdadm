0c7bc3ee91ac5313734684603d607c93b27e998f
Moshe Immerman <moshe.immerman@gmail.com>
2019-05-08 17:27:11 +0200

    fix: stash and clean git

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,11 +3,12 @@ NAME=$(basename $(git remote get-url origin | sed 's/\.git//'))
 GITHUB_USER=$(basename $(dirname $(git remote get-url origin | sed 's/\.git//')))
 TAG=$(git tag --points-at HEAD )
 
-[[ -e "checkout.txt" ]] && rm checkout.txt
-go get github.com/goreleaser/goreleaser
+GO111MODULE=off go get github.com/goreleaser/goreleaser
+git stash
+git clean -fd
 goreleaser release --rm-dist
 
-go get github.com/aktau/github-release
+GO111MODULE=off go get github.com/aktau/github-release
 github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME} -f dist/linux_amd64/${NAME}
 github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME}_osx -f dist/darwin_amd64/${NAME}
 github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME}.exe -f dist/windows_amd64/${NAME}.exe
